8e3f907e6b554431b78d5944c267dbac77b9ff9e
Ashique Ali <60208343+ashiqaliaslam@users.noreply.github.com>
2023-07-08 14:10:00 +0500

    inherited widget workshop

--- lib/main.dart
+++ lib/main.dart
@@ -1,270 +1,409 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const HorizonsApp());
+  runApp(
+    const AppStateWidget(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Store',
+        home: MyStorePage(),
+      ),
+    ),
+  );
 }
 
-class HorizonsApp extends StatelessWidget {
-  const HorizonsApp({Key? key}) : super(key: key);
+class AppState {
+  AppState({
+    required this.productList,
+    this.itemsInCart = const <String>{},
+  });
 
-  // This widget is the root of your application.
+  final List<String> productList;
+  final Set<String> itemsInCart;
+
+  AppState copyWith({
+    List<String>? productList,
+    Set<String>? itemsInCart,
+  }) {
+    return AppState(
+      productList: productList ?? this.productList,
+      itemsInCart: itemsInCart ?? this.itemsInCart,
+    );
+  }
+}
+
+class AppStateScope extends InheritedWidget {
+  const AppStateScope(this.data, {Key? key, required Widget child})
+      : super(key: key, child: child);
+
+  final AppState data;
+
+  static AppState of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<AppStateScope>()!.data;
+  }
+
+  @override
+  bool updateShouldNotify(AppStateScope oldWidget) {
+    return data != oldWidget.data;
+  }
+}
+
+class AppStateWidget extends StatefulWidget {
+  const AppStateWidget({required this.child, Key? key}) : super(key: key);
+
+  final Widget child;
+
+  static AppStateWidgetState of(BuildContext context) {
+    return context.findAncestorStateOfType<AppStateWidgetState>()!;
+  }
+
+  @override
+  AppStateWidgetState createState() => AppStateWidgetState();
+}
+
+class AppStateWidgetState extends State<AppStateWidget> {
+  AppState _data = AppState(
+    productList: Server.getProductList(),
+  );
+
+  void setProductList(List<String> newProductList) {
+    if (newProductList != _data.productList) {
+      setState(() {
+        _data = _data.copyWith(
+          productList: newProductList,
+        );
+      });
+    }
+  }
+
+  void addToCart(String id) {
+    if (!_data.itemsInCart.contains(id)) {
+      final Set<String> newItemsInCart = Set<String>.from(_data.itemsInCart);
+      newItemsInCart.add(id);
+      setState(() {
+        _data = _data.copyWith(
+          itemsInCart: newItemsInCart,
+        );
+      });
+    }
+  }
+
+  void removeFromCart(String id) {
+    if (_data.itemsInCart.contains(id)) {
+      final Set<String> newItemsInCart = Set<String>.from(_data.itemsInCart);
+      newItemsInCart.remove(id);
+      setState(() {
+        _data = _data.copyWith(
+          itemsInCart: newItemsInCart,
+        );
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      // This is the theme of your application.
-      theme: ThemeData.dark(),
-      // Scrolling in Flutter behaves differently depending on the
-      // ScrollBehavior. By default, ScrollBehavior changes depending
-      // on the current platform. For the purposes of this scrolling
-      // workshop, we're using a custom ScrollBehavior so that the
-      // experience is the same for everyone - regardless of the
-      // platform they are using.
-      scrollBehavior: const ConstantScrollBehavior(),
-      title: 'Horizons Weather',
-      home: Scaffold(
-        body: CustomScrollView(
-          slivers: <Widget>[
-            SliverAppBar(
-              pinned: true,
-              stretch: true,
-              onStretchTrigger: () async {
-                print('Load new data!');
-                // await Server.requestNewData();
-              },
-              backgroundColor: Colors.teal[800],
-              expandedHeight: 200.0,
-              flexibleSpace: FlexibleSpaceBar(
-                stretchModes: const [
-                  StretchMode.zoomBackground,
-                  StretchMode.fadeTitle,
-                  StretchMode.blurBackground,
-                ],
-                title: const Text('Horizons'),
-                background: DecoratedBox(
-                  position: DecorationPosition.foreground,
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      begin: Alignment.bottomCenter,
-                      end: Alignment.center,
-                      colors: <Color>[Colors.teal[800]!, Colors.transparent],
+    return AppStateScope(
+      _data,
+      child: widget.child,
+    );
+  }
+}
+
+class MyStorePage extends StatefulWidget {
+  const MyStorePage({Key? key}) : super(key: key);
+
+  @override
+  MyStorePageState createState() => MyStorePageState();
+}
+
+class MyStorePageState extends State<MyStorePage> {
+  bool _inSearch = false;
+  final TextEditingController _controller = TextEditingController();
+  final FocusNode _focusNode = FocusNode();
+
+  void _toggleSearch() {
+    setState(() {
+      _inSearch = !_inSearch;
+    });
+    AppStateWidget.of(context).setProductList(Server.getProductList());
+    _controller.clear();
+  }
+
+  void _handleSearch() {
+    _focusNode.unfocus();
+    final String filter = _controller.text;
+    AppStateWidget.of(context)
+        .setProductList(Server.getProductList(filter: filter));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            leading: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Image.network('$baseAssetURL/google-logo.png'),
+            ),
+            title: _inSearch
+                ? TextField(
+                    autofocus: true,
+                    focusNode: _focusNode,
+                    controller: _controller,
+                    onSubmitted: (_) => _handleSearch(),
+                    decoration: InputDecoration(
+                      hintText: 'Search Google Store',
+                      prefixIcon: IconButton(
+                        icon: const Icon(Icons.search),
+                        onPressed: _handleSearch,
+                      ),
+                      suffixIcon: IconButton(
+                        icon: const Icon(Icons.close),
+                        onPressed: _toggleSearch,
+                      ),
                     ),
-                  ),
-                  child: Image.network(
-                    headerImage,
-                    fit: BoxFit.cover,
-                  ),
+                  )
+                : null,
+            actions: [
+              if (!_inSearch)
+                IconButton(
+                  onPressed: _toggleSearch,
+                  icon: const Icon(Icons.search, color: Colors.black),
+                ),
+              const ShoppingCartIcon(),
+            ],
+            backgroundColor: Colors.white,
+            pinned: true,
+          ),
+          const SliverToBoxAdapter(
+            child: ProductListWidget(),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ShoppingCartIcon extends StatelessWidget {
+  const ShoppingCartIcon({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final Set<String> itemsInCart = AppStateScope.of(context).itemsInCart;
+    final bool hasPurchase = itemsInCart.isNotEmpty;
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        Padding(
+          padding: EdgeInsets.only(right: hasPurchase ? 17.0 : 10.0),
+          child: const Icon(
+            Icons.shopping_cart,
+            color: Colors.black,
+          ),
+        ),
+        if (hasPurchase)
+          Padding(
+            padding: const EdgeInsets.only(left: 17.0),
+            child: CircleAvatar(
+              radius: 8.0,
+              backgroundColor: Colors.lightBlue,
+              foregroundColor: Colors.white,
+              child: Text(
+                itemsInCart.length.toString(),
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 12.0,
                 ),
               ),
             ),
-            const WeeklyForecastList(),
-          ],
-        ),
-      ),
+          ),
+      ],
     );
   }
 }
 
-class WeeklyForecastList extends StatelessWidget {
-  const WeeklyForecastList({Key? key}) : super(key: key);
+class ProductListWidget extends StatelessWidget {
+  const ProductListWidget({Key? key}) : super(key: key);
+
+  void _handleAddToCart(String id, BuildContext context) {
+    AppStateWidget.of(context).addToCart(id);
+  }
+
+  void _handleRemoveFromCart(String id, BuildContext context) {
+    AppStateWidget.of(context).removeFromCart(id);
+  }
+
+  Widget _buildProductTile(String id, BuildContext context) {
+    return ProductTile(
+      product: Server.getProductById(id),
+      purchased: AppStateScope.of(context).itemsInCart.contains(id),
+      onAddToCart: () => _handleAddToCart(id, context),
+      onRemoveFromCart: () => _handleRemoveFromCart(id, context),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    final DateTime currentDate = DateTime.now();
-    final TextTheme textTheme = Theme.of(context).textTheme;
+    final List<String> productList = AppStateScope.of(context).productList;
+    return Column(
+      children:
+          productList.map((id) => _buildProductTile(id, context)).toList(),
+    );
+  }
+}
 
-    return SliverList(
-      delegate: SliverChildBuilderDelegate(
-        (context, index) {
-          final DailyForecast dailyForecast =
-              Server.getDailyForecastByID(index);
-          return Card(
-            child: Row(
-              children: <Widget>[
-                SizedBox(
-                  height: 200.0,
-                  width: 200.0,
-                  child: Stack(
-                    fit: StackFit.expand,
-                    children: <Widget>[
-                      DecoratedBox(
-                        position: DecorationPosition.foreground,
-                        decoration: BoxDecoration(
-                          gradient: RadialGradient(
-                            colors: <Color>[
-                              Colors.grey[800]!,
-                              Colors.transparent
-                            ],
-                          ),
-                        ),
-                        child: Image.network(
-                          dailyForecast.imageId,
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                      Center(
-                        child: Text(
-                          dailyForecast.getDate(currentDate.day).toString(),
-                          style: textTheme.displayLarge,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          dailyForecast.getWeekday(currentDate.weekday),
-                          style: textTheme.displaySmall,
-                        ),
-                        const SizedBox(height: 10.0),
-                        Text(dailyForecast.description),
-                      ],
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Text(
-                    '${dailyForecast.highTemp} | ${dailyForecast.lowTemp} F',
-                    style: textTheme.headlineSmall,
-                  ),
-                ),
-              ],
+class ProductTile extends StatelessWidget {
+  const ProductTile({
+    Key? key,
+    required this.product,
+    required this.purchased,
+    required this.onAddToCart,
+    required this.onRemoveFromCart,
+  }) : super(key: key);
+  final Product product;
+  final bool purchased;
+  final VoidCallback onAddToCart;
+  final VoidCallback onRemoveFromCart;
+
+  @override
+  Widget build(BuildContext context) {
+    Color getButtonColor(Set<MaterialState> states) {
+      return purchased ? Colors.grey : Colors.black;
+    }
+
+    BorderSide getButtonSide(Set<MaterialState> states) {
+      return BorderSide(
+        color: purchased ? Colors.grey : Colors.black,
+      );
+    }
+
+    return Container(
+      margin: const EdgeInsets.symmetric(
+        vertical: 15,
+        horizontal: 40,
+      ),
+      color: const Color(0xfff8f8f8),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: Text(product.title),
+          ),
+          Text.rich(
+            product.description,
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
             ),
-          );
-        },
-        childCount: 7,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: OutlinedButton(
+              style: ButtonStyle(
+                foregroundColor:
+                    MaterialStateProperty.resolveWith(getButtonColor),
+                side: MaterialStateProperty.resolveWith(getButtonSide),
+              ),
+              onPressed: purchased ? onRemoveFromCart : onAddToCart,
+              child: purchased
+                  ? const Text('Remove from cart')
+                  : const Text('Add to cart'),
+            ),
+          ),
+          Image.network(product.pictureURL),
+        ],
       ),
     );
   }
 }
 
-// --------------------------------------------
-// Below this line are helper classes and data.
+// The code below is for the dummy server, and you should not need to modify it
+// in this workshop.
 
 const String baseAssetURL =
-    'https://dartpad-workshops-io2021.web.app/getting_started_with_slivers/';
-const String headerImage = '${baseAssetURL}assets/header.jpeg';
+    'https://dartpad-workshops-io2021.web.app/inherited_widget/assets';
 
-const Map<int, DailyForecast> _kDummyData = {
-  0: DailyForecast(
-    id: 0,
-    imageId: '${baseAssetURL}assets/day_0.jpeg',
-    highTemp: 73,
-    lowTemp: 52,
-    description:
-        'Partly cloudy in the morning, with sun appearing in the afternoon.',
+const Map<String, Product> kDummyData = {
+  '0': Product(
+    id: '0',
+    title: 'Explore Pixel phones',
+    description: TextSpan(children: <TextSpan>[
+      TextSpan(
+        text: 'Capture the details.\n',
+        style: TextStyle(color: Colors.black),
+      ),
+      TextSpan(
+        text: 'Capture your world.',
+        style: TextStyle(color: Colors.blue),
+      ),
+    ]),
+    pictureURL: '$baseAssetURL/pixels.png',
   ),
-  1: DailyForecast(
-    id: 1,
-    imageId: '${baseAssetURL}assets/day_1.jpeg',
-    highTemp: 70,
-    lowTemp: 50,
-    description: 'Partly sunny.',
+  '1': Product(
+    id: '1',
+    title: 'Nest Audio',
+    description: TextSpan(children: <TextSpan>[
+      TextSpan(text: 'Amazing sound.\n', style: TextStyle(color: Colors.green)),
+      TextSpan(text: 'At your command.', style: TextStyle(color: Colors.black)),
+    ]),
+    pictureURL: '$baseAssetURL/nest.png',
   ),
-  2: DailyForecast(
-    id: 2,
-    imageId: '${baseAssetURL}assets/day_2.jpeg',
-    highTemp: 71,
-    lowTemp: 55,
-    description: 'Party cloudy.',
+  '2': Product(
+    id: '2',
+    title: 'Nest Audio Entertainment packages',
+    description: TextSpan(children: <TextSpan>[
+      TextSpan(
+        text: 'Built for music.\n',
+        style: TextStyle(color: Colors.orange),
+      ),
+      TextSpan(
+        text: 'Made for you.',
+        style: TextStyle(color: Colors.black),
+      ),
+    ]),
+    pictureURL: '$baseAssetURL/nest-audio-packages.png',
   ),
-  3: DailyForecast(
-    id: 3,
-    imageId: '${baseAssetURL}assets/day_3.jpeg',
-    highTemp: 74,
-    lowTemp: 60,
-    description: 'Thunderstorms in the evening.',
-  ),
-  4: DailyForecast(
-    id: 4,
-    imageId: '${baseAssetURL}assets/day_4.jpeg',
-    highTemp: 67,
-    lowTemp: 60,
-    description: 'Severe thunderstorm warning.',
-  ),
-  5: DailyForecast(
-    id: 5,
-    imageId: '${baseAssetURL}assets/day_5.jpeg',
-    highTemp: 73,
-    lowTemp: 57,
-    description: 'Cloudy with showers in the morning.',
-  ),
-  6: DailyForecast(
-    id: 6,
-    imageId: '${baseAssetURL}assets/day_6.jpeg',
-    highTemp: 75,
-    lowTemp: 58,
-    description: 'Sun throughout the day.',
+  '3': Product(
+    id: '3',
+    title: 'Nest Home Security packages',
+    description: TextSpan(children: <TextSpan>[
+      TextSpan(text: 'Your home,\n', style: TextStyle(color: Colors.black)),
+      TextSpan(text: 'safe and sound.', style: TextStyle(color: Colors.red)),
+    ]),
+    pictureURL: '$baseAssetURL/nest-home-packages.png',
   ),
 };
 
 class Server {
-  static List<DailyForecast> getDailyForecastList() =>
-      _kDummyData.values.toList();
+  static Product getProductById(String id) {
+    return kDummyData[id]!;
+  }
 
-  static DailyForecast getDailyForecastByID(int id) {
-    assert(id >= 0 && id <= 6);
-    return _kDummyData[id]!;
+  static List<String> getProductList({String? filter}) {
+    if (filter == null) return kDummyData.keys.toList();
+    final List<String> ids = <String>[];
+    for (final Product product in kDummyData.values) {
+      if (product.title.toLowerCase().contains(filter.toLowerCase())) {
+        ids.add(product.id);
+      }
+    }
+    return ids;
   }
 }
 
-class DailyForecast {
-  const DailyForecast({
+class Product {
+  const Product({
     required this.id,
-    required this.imageId,
-    required this.highTemp,
-    required this.lowTemp,
+    required this.pictureURL,
+    required this.title,
     required this.description,
   });
 
-  final int id;
-  final String imageId;
-  final int highTemp;
-  final int lowTemp;
-  final String description;
-
-  static const List<String> _weekdays = <String>[
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday',
-    'Saturday',
-    'Sunday',
-  ];
-
-  String getWeekday(int today) {
-    final int offset = today + id;
-    final int day = offset >= 7 ? offset - 7 : offset;
-    return _weekdays[day];
-  }
-
-  int getDate(int today) => today + id;
-}
-
-class ConstantScrollBehavior extends ScrollBehavior {
-  const ConstantScrollBehavior();
-
-  @override
-  Widget buildScrollbar(
-          BuildContext context, Widget child, ScrollableDetails details) =>
-      child;
-
-  @override
-  Widget buildOverscrollIndicator(
-          BuildContext context, Widget child, ScrollableDetails details) =>
-      child;
-
-  @override
-  TargetPlatform getPlatform(BuildContext context) => TargetPlatform.macOS;
-
-  @override
-  ScrollPhysics getScrollPhysics(BuildContext context) =>
-      const BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics());
+  final String id;
+  final String pictureURL;
+  final String title;
+  final TextSpan description;
 }
